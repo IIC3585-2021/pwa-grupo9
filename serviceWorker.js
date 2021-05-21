dcba128991f39f8e678b31b0d2e25a13d84e490e
Pablo Garnham <pgarnham@uc.cl>
2021-05-21 13:34:36 -0400

    changing firebase version in links

--- serviceWorker.js
+++ serviceWorker.js
@@ -32,8 +32,8 @@ self.addEventListener("fetch", fetchEvent => {
 });
 
 
-importScripts('https://www.gstatic.com/firebasejs/8.4.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.4.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/8.6.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.6.2/firebase-messaging.js');
 
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
